7e3ff57f7f7a9d60c493903392b6031b784c663b
Tharun <tharungarena@gmail.com>
2026-09-10 19:09:34 +0530

    added all the required apps

--- ClinicManagement/ClinicManagement/settings.py
+++ ClinicManagement/ClinicManagement/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apibackendapp'
+    # Core app
+    'apibackendapp',
+    # Role-based apps
+    'doctor',
+    'receptionist',
+    'adminapp',
+    'lab_tech',
+    'pharmacy',
 ]
 
 MIDDLEWARE = [
